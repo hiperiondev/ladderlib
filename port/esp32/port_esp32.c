37d9fb2de58fe1f1930e8cd2435d11f841f70360
hiperiondev <egonzalez.hiperion@gmail.com>
2025-03-12 21:17:03 -0300

    ESP32 port: Correct input test

--- port/esp32/port_esp32.c
+++ port/esp32/port_esp32.c
@@ -44,37 +44,39 @@
 static const char *TAG = "port_esp32";
 
 static const uint32_t inputs[] = {
-        INPUT_00, //
-        INPUT_01, //
-        INPUT_02, //
-        INPUT_03, //
-        INPUT_04, //
-        INPUT_05, //
-        INPUT_06, //
-        INPUT_07, //
-        };
+    INPUT_00, //
+    INPUT_01, //
+    INPUT_02, //
+    INPUT_03, //
+    INPUT_04, //
+    INPUT_05, //
+    INPUT_06, //
+    INPUT_07, //
+};
 
 static const uint32_t outputs[] = {
-        OUTPUT_00, //
-        OUTPUT_01, //
-        OUTPUT_02, //
-        OUTPUT_03, //
-        OUTPUT_04, //
-        OUTPUT_05, //
-        };
+    OUTPUT_00, //
+    OUTPUT_01, //
+    OUTPUT_02, //
+    OUTPUT_03, //
+    OUTPUT_04, //
+    OUTPUT_05, //
+};
 
 bool esp32_port_initialize(ladder_ctx_t *ladder_ctx) {
     gpio_config_t GPIO_Config;
 
-    if ((*ladder_ctx).ladder.quantity.i > 8 - (*ladder_ctx).ladder.quantity.iw || (*ladder_ctx).ladder.quantity.q > 6 - (*ladder_ctx).ladder.quantity.qw
-            || (*ladder_ctx).ladder.quantity.iw > 2 || (*ladder_ctx).ladder.quantity.qw > 2) {
+    if ((*ladder_ctx).ladder.quantity.i > 8 - (*ladder_ctx).ladder.quantity.iw || (*ladder_ctx).ladder.quantity.q > 6 - (*ladder_ctx).ladder.quantity.qw ||
+        (*ladder_ctx).ladder.quantity.iw > 2 || (*ladder_ctx).ladder.quantity.qw > 2) {
         ESP_LOGI(TAG, "ERROR quantity");
         return false;
     }
 
-    GPIO_Config.pin_bit_mask = (1ULL << INPUT_01) | (1ULL << INPUT_02) | (1ULL << INPUT_03) | (1ULL << INPUT_04) | (1ULL << INPUT_05)
-            | ((*ladder_ctx).ladder.quantity.iw > 0 ? 0 : (1ULL << INPUT_06)) | ((*ladder_ctx).ladder.quantity.iw > 1 ? 0 : (1ULL << INPUT_07));
+    GPIO_Config.pin_bit_mask = (1ULL << INPUT_00) | (1ULL << INPUT_01) | (1ULL << INPUT_02) | (1ULL << INPUT_03) | (1ULL << INPUT_04) | (1ULL << INPUT_05) |
+                               ((*ladder_ctx).ladder.quantity.iw > 0 ? 0 : (1ULL << INPUT_06)) |
+                               ((*ladder_ctx).ladder.quantity.iw > 1 ? 0 : (1ULL << INPUT_07));
     GPIO_Config.pull_up_en = GPIO_PULLUP_DISABLE;
+    GPIO_Config.pull_down_en = GPIO_PULLDOWN_DISABLE;
     GPIO_Config.mode = GPIO_MODE_INPUT;
     GPIO_Config.intr_type = GPIO_INTR_DISABLE;
     if (gpio_config(&GPIO_Config) != ESP_OK) {
@@ -82,8 +84,9 @@ bool esp32_port_initialize(ladder_ctx_t *ladder_ctx) {
         return false;
     }
 
-    GPIO_Config.pin_bit_mask = (1ULL << OUTPUT_01) | (1ULL << OUTPUT_02) | (1ULL << OUTPUT_03)
-            | ((*ladder_ctx).ladder.quantity.qw > 0 ? 0 : (1ULL << OUTPUT_04)) | ((*ladder_ctx).ladder.quantity.qw > 1 ? 0 : (1ULL << OUTPUT_05));
+    GPIO_Config.pin_bit_mask = (1ULL << OUTPUT_00) | (1ULL << OUTPUT_01) | (1ULL << OUTPUT_02) | (1ULL << OUTPUT_03) |
+                               ((*ladder_ctx).ladder.quantity.qw > 0 ? 0 : (1ULL << OUTPUT_04)) |
+                               ((*ladder_ctx).ladder.quantity.qw > 1 ? 0 : (1ULL << OUTPUT_05));
     GPIO_Config.pull_down_en = GPIO_PULLDOWN_DISABLE;
     GPIO_Config.pull_up_en = GPIO_PULLUP_DISABLE;
     GPIO_Config.mode = GPIO_MODE_OUTPUT;
@@ -96,6 +99,62 @@ bool esp32_port_initialize(ladder_ctx_t *ladder_ctx) {
     return true;
 }
 
+void esp32_port_output_test(void) {
+    ladder_ctx_t ladder_ctx;
+
+    ladder_ctx.ladder.quantity.i = 8;
+    ladder_ctx.ladder.quantity.q = 6;
+    ladder_ctx.ladder.quantity.iw = 0;
+    ladder_ctx.ladder.quantity.qw = 0;
+
+    if (!esp32_port_initialize(&ladder_ctx)) {
+        ESP_LOGI(TAG, "ERROR esp32 port initialize");
+        return;
+    }
+
+    for (uint32_t qs = 0; qs < ladder_ctx.ladder.quantity.q; qs++) {
+        ESP_LOGI(TAG, "test port %d", (int)qs);
+        gpio_set_level(outputs[qs], 1);
+        esp32_delay(1000);
+        gpio_set_level(outputs[qs], 0);
+    }
+}
+
+void esp32_port_input_test(void) {
+    uint64_t start = 0;
+
+    ladder_ctx_t ladder_ctx;
+    bool port = false;
+    bool ports[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+
+    ladder_ctx.ladder.quantity.i = 8;
+    ladder_ctx.ladder.quantity.q = 6;
+    ladder_ctx.ladder.quantity.iw = 0;
+    ladder_ctx.ladder.quantity.qw = 0;
+
+    if (!esp32_port_initialize(&ladder_ctx)) {
+        ESP_LOGI(TAG, "ERROR esp32 port initialize");
+        return;
+    }
+
+    ESP_LOGI(TAG, "(Stop after 10 sec of no input)");
+    start = esp32_millis();
+    while (1) {
+        for (uint32_t is = 0; is < ladder_ctx.ladder.quantity.i; is++) {
+            port = gpio_get_level(inputs[is]);
+            if (port != ports[is]) {
+                ports[is] = port;
+                ESP_LOGI(TAG, "PORT %d %s", (int)is, (port == 0 ? "OFF" : "ON"));
+                start = esp32_millis();
+            }
+        }
+        esp32_delay(1);
+
+        if (esp32_millis() - start > 10000)
+            break;
+    }
+}
+
 void esp32_delay(long msec) {
     vTaskDelay(msec / portTICK_PERIOD_MS);
 }
@@ -105,7 +164,7 @@ uint64_t esp32_millis(void) {
 }
 
 void esp32_read_inputs_local(ladder_ctx_t *ladder_ctx) {
-    for (uint32_t is = 0; is < (*ladder_ctx).ladder.quantity.q; is++)
+    for (uint32_t is = 0; is < (*ladder_ctx).ladder.quantity.i; is++)
         (*ladder_ctx).memory.I[is] = gpio_get_level(inputs[is]);
 }
 
--- port/esp32/port_esp32.h
+++ port/esp32/port_esp32.h
@@ -36,26 +36,22 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-
 #include "ladder.h"
 
 #define INPUT_00     GPIO_NUM_34
 #define INPUT_01     GPIO_NUM_35
 #define INPUT_02     GPIO_NUM_32
 #define INPUT_03     GPIO_NUM_13
-#define INPUT_04     GPIO_NUM_4
+#define INPUT_04     GPIO_NUM_15
 #define INPUT_05     GPIO_NUM_0
 #define INPUT_06     GPIO_NUM_39
 #define INPUT_07     GPIO_NUM_36
 #define AN_INPUT_00  GPIO_NUM_39
 #define AN_INPUT_01  GPIO_NUM_36
-
-#define OUTPUT_00    GPIO_NUM_17
+#define OUTPUT_00    GPIO_NUM_4
 #define OUTPUT_01    GPIO_NUM_16
 #define OUTPUT_02    GPIO_NUM_2
-#define OUTPUT_03    GPIO_NUM_15
+#define OUTPUT_03    GPIO_NUM_17
 #define OUTPUT_04    GPIO_NUM_26
 #define OUTPUT_05    GPIO_NUM_25
 #define AN_OUTPUT_00 GPIO_NUM_26
@@ -69,6 +65,18 @@
  * @return
  */
 bool esp32_port_initialize(ladder_ctx_t *ladder_ctx);
+
+/**
+ * @fn void esp32_port_output_test(void)
+ * @brief
+ */
+void esp32_port_output_test(void);
+
+/**
+ * @fn void esp32_port_input_test(void)
+ * @brief
+ */
+void esp32_port_input_test(void);
 
 /**
  * @fn void esp32_read_inputs_local(ladder_ctx_t*)
