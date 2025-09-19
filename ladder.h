f732a8b39d32f4821c2eceb74b4f438db2c7f684
hiperiondev <egonzalez.hiperion@gmail.com>
2025-09-19 18:24:38 -0300

    Udate version

--- ladder.h
+++ ladder.h
@@ -49,7 +49,7 @@
  * @def LADDERLIB_VERSION_MINOR
  * @brief Indicate some change on API or opcode or very important correction in functionality
  */
-#define LADDERLIB_VERSION_MINOR 2
+#define LADDERLIB_VERSION_MINOR 3
 
 /**
  * @def LADDERLIB_VERSION_PATCH
