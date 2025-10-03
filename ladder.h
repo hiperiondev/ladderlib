da12273acd62eaba240e3fd79c78ed27f3319a10
hiperiondev <egonzalez.hiperion@gmail.com>
2025-10-03 11:25:47 -0300

    Update version

--- ladder.h
+++ ladder.h
@@ -49,7 +49,7 @@
  * @def LADDERLIB_VERSION_MINOR
  * @brief Indicate some change on API or opcode or very important correction in functionality
  */
-#define LADDERLIB_VERSION_MINOR 3
+#define LADDERLIB_VERSION_MINOR 4
 
 /**
  * @def LADDERLIB_VERSION_PATCH
